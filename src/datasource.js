254f8eed359e40677a90e510c3a1c7bca5617060
Deltanic <rdg94@live.nl>
2018-04-25 14:40:44 +0200

    Visualize query results.

--- src/datasource.js
+++ src/datasource.js
@@ -3,28 +3,62 @@ export default class Datasource {
     this.name = instanceSettings.name;
     this.id = instanceSettings.id;
 
+    this.url = 'http://localhost:8080';
+
     this.$q = $q;
     this.backendSrv = backendSrv;
     this.templateSrv = templateSrv;
   }
 
+  // ---------------------------------------------------------------------------
+
+  doRequest = query => this.backendSrv.datasourceRequest({
+    url: `${this.url}/query_exp?query=${query}&date_format=js`,
+    method: 'GET',
+  });
+
+  transformResponse = (response) => {
+    const target = 'select metric';
+    const datapoints = response.data.tables[0].results
+      .map(([date, sum]) => [sum, Date.parse(date)]);
+
+    return { target, datapoints };
+  };
+
+  transformAll = (results) => {
+    const data = results.map(this.transformResponse);
+    return { data };
+  };
+
+  // ---------------------------------------------------------------------------
+
   query(options) {
-    console.log('QUERY OPTIONS', options);
-    throw new Error('Query Support not implemented yet.');
+    const queries = options.targets
+      .filter(x => !x.hide)
+      .filter(x => x.rawSql)
+      .map(x => x.rawSql);
+
+    if (!queries.length) {
+      const data = [];
+      return this.$q.when({ data });
+    }
+
+    const requests = queries.map(this.doRequest);
+    return Promise.all(requests)
+      .then(this.transformAll);
   }
 
   annotationQuery(options) {
-    console.log('ANNOTIATION OPTIONS', options);
-    throw new Error('Annotation Support not implemented yet.');
+    console.log('annotationQuery:', JSON.parse(JSON.stringify(options)));
+    throw new Error('annotationQuery is not yet implemented.');
   }
 
   metricFindQuery(query) {
-    console.log('METRIC FIND QUERY', query);
-    throw new Error('Template Variable Support not implemented yet.');
+    console.log('metricFindQuery:', JSON.parse(JSON.stringify(query)));
+    throw new Error('metricFindQuery is not yet implemented.');
   }
 
   testDatasource() {
-    console.log('TEST DATASOURCE');
     return this.$q.when({
       status: 'success',
       message: 'Data source is working',
