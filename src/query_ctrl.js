f87416f4bd034b8ddaaf1cc531f7d24fceb4e9af
Leon Mergen <leon@solatis.com>
2018-06-25 15:10:03 +0200

    Fix default query

--- src/query_ctrl.js
+++ src/query_ctrl.js
@@ -3,8 +3,9 @@ import { QueryCtrl as GrafanaQueryCtrl } from 'app/plugins/sdk';
 export default class QueryCtrl extends GrafanaQueryCtrl {
   static templateUrl = 'partials/query.editor.html';
   // static defaultQuery = 'select 1 from test in range (2000, 2020)';
-  // static defaultQuery = 'select sum(bidSize) from test in $__range group by $__interval';
-  static defaultQuery = 'select "open" from "stocks.goldman_sachs" in $__range';
+    // static defaultQuery = 'select sum(bidSize) from test in $__range group by $__interval';
+    // static defaultQuery = 'select min(byteSendRate) from "network_baseline_network_windows_lons111003_infra" in $__range group by $__interval'
+  static defaultQuery = 'select min(open) from "stocks.goldman_sachs" in $__range group by $__interval';
 
   constructor($scope, $injector) {
     super($scope, $injector);
