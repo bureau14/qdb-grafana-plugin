5579f58d4ec42ef2456794d9ed5800a55ad108a6
vianney <vianney.p@outlook.com>
2021-02-23 16:07:17 +0100

    Add range variable support.

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,34 +1,37 @@
-import { DataSourceInstanceSettings } from '@grafana/data';
+import { DataSourceInstanceSettings, TimeRange } from '@grafana/data';
 import { DataSourceWithBackend } from '@grafana/runtime';
-import { ScopedVars } from '@grafana/data';
+import { DataQueryRequest, DataQueryResponse } from '@grafana/data';
 import { QdbDataSourceOptions, QdbQuery } from './types';
 import { getTemplateSrv } from '@grafana/runtime';
+import { Observable } from 'rxjs';
 
 export class DataSource extends DataSourceWithBackend<QdbQuery, QdbDataSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<QdbDataSourceOptions>) {
     super(instanceSettings);
   }
-  applyTemplateVariables(query: QdbQuery, scopedVars: ScopedVars) {
+
+  replaceRange(range: TimeRange, toReplace: string, queryText: string) {
+    return queryText?.replace(
+      toReplace,
+      `RANGE(${range.from.utc().format('YYYY-MM-DD[T]HH:mm:ss.SSSSSSSSS')}, ${range.to
+        .utc()
+        .format('YYYY-MM-DD[T]HH:mm:ss.SSSSSSSSS')})`
+    );
+  }
+
+  query(request: DataQueryRequest<QdbQuery>): Observable<DataQueryResponse> {
+    request.targets.map(x => (x.queryText = this.replaceRange(request.range, '$__range', x.queryText ?? '')));
+    request.targets.map(x => (x.queryText = this.replaceRange(request.range, '${__range}', x.queryText ?? '')));
+    console.log(`request.targets[0].queryText: ${request.targets[0].queryText}`);
+
+    return super.query(request);
+  }
+
+  applyTemplateVariables(query: QdbQuery) {
     const templateSrv = getTemplateSrv();
-    console.log(`interval: ${this.interval}`);
-    console.log(`scopedVars: ${scopedVars[0]}`);
-    console.log(`scopedVars: ${scopedVars.text}`);
-    let queryText = templateSrv.replace(query.queryText ?? '', scopedVars);
-    console.log(`query: ${queryText}`);
-
-    // const from = scopedVars.find(({ name }) => name === '__from');
-    // const to = scopedVars.find(({ name }) => name === '__to');
-    // console.log(`from: ${from}`);
-    // console.log(`to: ${to}`);
-    // const range = `range(${from}, ${to})`;
-    // queryText = queryText.replace('$__range', range);
-    // queryText = queryText.replace('${__range}', range);
-
-    console.log(`query: ${queryText}`);
-
     return {
       ...query,
-      queryText,
+      queryText: templateSrv.replace(query.queryText ?? ''),
     };
   }
 }
