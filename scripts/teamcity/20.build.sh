32756db53ae2ec702991799065bcbb24533aa805
vianney <vianney.p@outlook.com>
2021-03-16 16:05:39 +0100

    Add teamcity build scripts.

---
+++ scripts/teamcity/20.build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eux
+
+SCRIPT_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+source "$SCRIPT_DIR/configure.sh"
+
+
+rm -Rf dist
+npm run build
---
+++ scripts/teamcity/configure.sh
@@ -0,0 +1,7 @@
+set -eux
+
+SCRIPT_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+source "$SCRIPT_DIR/commands.sh"
+
+nvm_use
+npm_config
