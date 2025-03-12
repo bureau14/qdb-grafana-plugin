8f573b525dd10de6c2eeaaf1e595071ebe8a6892
Leon Mergen <leon@solatis.com>
2025-03-12 15:14:57 +0700

    Attempt to fix things to not use nvm

--- scripts/teamcity/configure.sh
+++ scripts/teamcity/configure.sh
@@ -1,7 +1,18 @@
+#!/usr/bin/env bash
+
 set -eux
 
 SCRIPT_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
-source "$SCRIPT_DIR/commands.sh"
 
-nvm_use
-npm_config
+NPM=$(command -v npm)
+
+case "$(uname)" in
+    MINGW*)
+        ${NPM} config set msvs_version 2017
+        ;;
+    *)
+        ${NPM} config set python python3
+        ;;
+esac
+
+export NPM="${NPM}"
