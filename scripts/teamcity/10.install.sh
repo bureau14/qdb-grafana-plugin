90612d5a37cb65a741d50059c9c0cb768f3c6fb5
Leon Mergen <leon@solatis.com>
2025-03-12 17:52:02 +0700

    QDB-16066 - Fix Node.JS / Grafana build (#22)
    Node.JS has been done completely differently in the Debian agents / Teamcity, this commit addresses that.

--- scripts/teamcity/10.install.sh
+++ scripts/teamcity/10.install.sh
@@ -3,24 +3,13 @@
 set -eux
 
 SCRIPT_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
-source "$SCRIPT_DIR/commands.sh"
-
-echo "Installing version: $NODE_VERSION -- arch: $NODE_ARCH";
-
-nvm_load
-nvm install $NODE_VERSION $NODE_ARCH
-
 source "$SCRIPT_DIR/configure.sh"
-
-nvm_use
-npm_config
 
 rm -Rf $GOPATH || true
 
 mkdir $GOPATH || true
 rm $GOPATH/go.mod || true
 
-${NPM} install --global yarn
 ${YARN} install
 
 rm -Rf $GOPATH/src/github.com/magefile
--- scripts/teamcity/configure.sh
+++ scripts/teamcity/configure.sh
@@ -1,7 +1,27 @@
+#!/usr/bin/env bash
+
 set -eux
 
 SCRIPT_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
-source "$SCRIPT_DIR/commands.sh"
 
-nvm_use
-npm_config
+# Commands we expect to exist
+NODEJS=${NODEJS_CMD:-"nodejs"}
+NODE_GYP=${NODE_GYP_CMD:-"node-gyp"}
+NPM=${NPM_CMD:-"npm"}
+YARN=${YARN_CMD:-"yarn"}
+
+case "$(uname)" in
+    MINGW*)
+        ${NPM} config set msvs_version 2017
+        ;;
+    *)
+        echo "Nothing to do"
+        ;;
+esac
+
+export NODEJS="${NODEJS}"
+export NODE_GYP="${NODE_GYP}"
+export NPM="${NPM}"
+export YARN="${YARN}"
+
+export NODE_OPTIONS=--openssl-legacy-provider
