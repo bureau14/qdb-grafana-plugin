6b7a58c2408d5e95c515be177d1cccc3aa92a265
Igor <igor.niebylski@gmail.com>
2026-07-28 12:40:42 +0200

    move

--- scripts/cicd/30.sign.sh
+++ scripts/cicd/30.sign.sh
@@ -6,3 +6,4 @@ SCRIPT_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 source "$SCRIPT_DIR/common.sh"
 
 ${YARN} sign --rootUrls https://quasardb.net
+npx @grafana/sign-plugin@3.2.0
