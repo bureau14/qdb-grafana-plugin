0b5741389b55aa4c87145fc748f7d86ee36c5ca7
Igor <igor.niebylski@gmail.com>
2026-07-30 12:38:07 +0200

    finalize

--- scripts/cicd/40.package.sh
+++ scripts/cicd/40.package.sh
@@ -5,12 +5,8 @@ set -eu
 rm qdb-grafana-plugin.zip || true
 mv dist quasardb-datasource
 
-# The validator requires directories to be traversable and regular files to be at least 0744.
+# The validator requires directories to be traversable and regular files to be at least 0744
 find quasardb-datasource -type d -exec chmod 0755 {} +
 find quasardb-datasource -type f -exec chmod 0744 {} +
 find quasardb-datasource -maxdepth 1 -type f -name 'gpx_qdb-grafana-plugin_*' -exec chmod 0755 {} +
-echo 'Plugin executable permissions before packaging:'
-find quasardb-datasource -maxdepth 1 -type f -name 'gpx_qdb-grafana-plugin_*' -printf '%m %p\n'
 zip -vr qdb-grafana-plugin quasardb-datasource
-echo 'Plugin executable permissions in archive:'
-zipinfo -l qdb-grafana-plugin.zip 'quasardb-datasource/gpx_qdb-grafana-plugin_*'
